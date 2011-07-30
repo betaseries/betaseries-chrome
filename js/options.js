de3c87623b0ea497157f5e60c77041eabd6bc696
Menencia <menencia@gmail.com>
2011-07-30 10:56:56 +0400

    fix options page

--- js/options.js
+++ js/options.js
@@ -2,8 +2,6 @@ $(document).ready(function(){
 
 	var bgPage = chrome.extension.getBackgroundPage();
 	
-	var options = JSON.parse(localStorage.options);
-	
 	/**
 	 * Internationalisation
 	 */
@@ -18,20 +16,17 @@ $(document).ready(function(){
 	$('#display_global').text(__("display_global"));
 	
 	// Remplissage des champs
-	$('select[name=badge_notification_type]').val(options['badge_notification_type']);
-	$('select[name=dl_srt_language]').val(options['dl_srt_language']);
-	$('input[name=nbr_episodes_per_serie]').attr('value', options['nbr_episodes_per_serie']);
-	$('select[name=display_global]').val(options['display_global']);
+	$('select[name=badge_notification_type]').val(DB.get('options.badge_notification_type'));
+	$('select[name=dl_srt_language]').val(DB.get('options.dl_srt_language'));
+	$('input[name=nbr_episodes_per_serie]').attr('value', DB.get('options.nbr_episodes_per_serie'));
+	$('select[name=display_global]').val(DB.get('options.display_global'));
 	
 	$('#save').click(function(){
-		options = {
-			badge_notification_type: $('select[name=badge_notification_type] :selected').val(),
-			dl_srt_language: $('select[name=dl_srt_language] :selected').val(),
-			nbr_episodes_per_serie: $('input[name=nbr_episodes_per_serie]').attr('value'),
-			display_global: $('select[name=display_global] :selected').val()
-		};
-		localStorage.options = JSON.stringify(options);
-		bgPage.updateBadge();
+		DB.set('options.badge_notification_type', $('select[name=badge_notification_type] :selected').val());
+		DB.set('options.dl_srt_language', $('select[name=dl_srt_language] :selected').val());
+		DB.set('options.nbr_episodes_per_serie', $('input[name=nbr_episodes_per_serie]').attr('value'));
+		DB.set('options.display_global', $('select[name=display_global] :selected').val());
+		bgPage.badge.update();
 		$(this).html('Sauvegardé !');
 		$(this).css('background-color', '#eafedf');
 		$('#save').css('color', '#999');
